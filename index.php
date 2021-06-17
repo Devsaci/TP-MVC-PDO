9582d322348838bc7fc9b3d61f01e008d92d2ae6
saci zakaria <sacideveloppeur@gmail.com>
2021-06-17 21:08:15 +0200

    TP MVC PDO

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php session_start(); 
+include("modeles/Continent.php");
+include("modeles/monPdo.php");
+include("vues/header.php");
+include("vues/messagesFlash.php");
+
+$uc =empty($_GET['uc']) ? "accueil" : $_GET['uc'];
+
+switch($uc){
+case 'accueil' :
+include('vues/accueil.php');
+break;
+case 'continents' :
+include('Controllers/continentController.php');
+break;
+}
+include("vues/footer.php");
+?>
